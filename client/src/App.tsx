f3a35c9b956689f76e88443319b241c9aa62aa16
ted-jv <wkejrdldls@gmail.com>
2023-01-15 23:44:20 +0900

    Feat: Add routing 처리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { Routes, BrowserRouter, Route } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import Store from './pages/Store';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/store" element={<Store />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
