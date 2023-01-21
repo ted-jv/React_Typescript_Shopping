623fafd1a95fdf5ad666e4cb32fc64cc1cf038e4
ted-jv <wkejrdldls@gmail.com>
2023-01-21 23:42:51 +0900

    Feat: Add StoreItem basic HTML

--- client/src/components/StoreItem.tsx
+++ client/src/components/StoreItem.tsx
@@ -6,12 +6,26 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
+  const quantity = 0;
   return (
     <div className="card">
       <img src={imgUrl} alt={name} className="card-img" />
       <body>
-        <span>{name}</span>
+        <title>{name}</title>
         <span>{price}</span>
+        <div className="add-btn">
+          {quantity === 0 ? (
+            <button>+ Add To Cart</button>
+          ) : (
+            <div>
+              <button>-</button>
+              <div>
+                <span>{quantity}</span>in cart
+              </div>
+              <button>+</button>
+            </div>
+          )}
+        </div>
       </body>
     </div>
   );
